7f7e3b60b89c49ad9656074c77396e1ecc7883a8
Josguther321 <josguther.14@gmail.com>
2020-03-14 21:40:31 -0600

    Landing page

---
+++ index.php
@@ -0,0 +1,417 @@
+<!doctype html>
+<html class="no-js" lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <title>Nombre de tu Colegio Aqui</title>
+
+    <meta name="description" content="Ova is elegant and beautiful modern app lading website">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Favicon -->
+    <link rel="icon" href="images/favicon/favicon.png">
+    <link rel="apple-touch-icon" href="images/favicon/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/favicon/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/favicon/apple-touch-icon-114x114.png">
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="css/vendor/owl.carousel.css">
+    <link rel="stylesheet" href="css/vendor/owl.theme.css">
+    <link rel="stylesheet" href="css/vendor/venobox.css">
+    <link rel="stylesheet" href="css/vendor/elegant-icons.css">
+    <link rel="stylesheet" href="css/vendor/pe-icon-7-stroke.min.css">
+    <link rel="stylesheet" href="css/vendor/font-awesome.css">
+    <link rel="stylesheet" href="css/vendor/animate.css">
+    <link rel="stylesheet" href="css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" href="css/base.css">
+</head>
+<body>
+    <!-- ############################ HEADER ############################ -->
+    <header id="home">
+
+     <?php /*   <div id="main-menu">
+
+            <nav class="side-navigation-menu clearfix">
+                <div class="close-btn clearfix">
+                    <p class="pull-left">Menu de Navegacion</p>
+                    <a href="#" class="main-menu-close pull-right"><i class="icon_close"></i></a>
+                </div>
+                <ul>
+                   
+                    <li><a href="http://classlearngt.info/moodle/login/index.php">Iniciar Sesion</a></li>
+                </ul>
+
+            </nav><!-- menu-area -->
+           
+
+           
+        </div><!-- #main-menu --> */ ?>
+
+
+        <div class="overlay-light">
+
+            <div class="container">
+
+                <div class="menu-container">
+
+                    <div class="row">
+
+                        <div class="col-xs-6">
+
+                            <div class="logo">
+                                <a href=""><img src="images/logo/logo2.jpg" 
+                                style="border-radius: 10px;"
+                                height="40" width="40" alt="Live Logo">
+                                </a>
+                            </div><!-- .logo -->
+
+                        </div><!-- .col-xs-6 -->
+
+                        <div class="col-xs-3" style="margin-left: 25%;">
+
+                          <?php /*  <nav class='fr hamburger-menu'>
+
+                                <a href="#" class="main-menu-open">
+                                    <i class="icon_menu"></i>
+                                </a>
+                                
+
+                            </nav><!-- nav --> */?>
+                            <div class="header-buttons">
+                                    <a class="btn-white" href="http://classlearngt.info/moodle/login/index.php">Iniciar Sesion</a>
+                                    
+                                </div><!-- header-buttons -->
+                        </div><!-- .col-xs-6 -->
+
+                    </div><!-- .row -->
+
+                </div><!-- .menu-container -->
+
+                <div class="header-container">
+
+                    <div class="row">
+
+                        <div class="col-md-12">
+
+                            <div class="header-items">
+
+                                <div class="header-text">
+                                    <h1><span>Tu Colegio</span>  Bienvenido  <br>Informate sobre nuestras carreras</h1>
+                                </div><!-- header-text -->
+
+                                <div class="header-buttons">
+                                    <a class="btn-white" href="#">Conocenos</a>
+                                    <a class="btn-white-outlined" href="#">Ver Carreras</a>
+                                </div><!-- header-buttons -->
+
+                                <div class="app-image-container">
+                                    <img src="images/header/app-image.png" alt="App Images">
+                                </div> <!-- app-image-container -->
+
+                            </div> <!-- .header-text -->
+
+                        </div><!-- .col-md-12 -->
+
+                    </div><!-- .row -->
+
+                </div><!-- .header-container -->
+
+            </div><!-- .container -->
+
+        </div><!-- .overlay -->
+
+    </header><!-- #home -->
+
+
+
+    <!-- ############################ Feature ############################ -->
+    <section id="features" class="section light">
+
+        <div class="section-heading">
+
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="col-md-12">
+
+                        <div class="section-heading-container">
+                            <h2>
+                                <span>Sobre Nosotros</span>
+                            </h2>
+                            <p>
+                            Somos una institucion educativa con más de 15 años de experiencia brindando
+                             educación de calidad a niños y niñas de Guatemala.
+                            </p>
+                        </div><!-- section-header-container -->
+
+                    </div><!-- col-md-12 -->
+
+                </div><!-- row -->
+
+            </div><!-- container -->
+
+        </div><!-- section-heading -->
+
+
+        <div class="section-content">
+
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="col-md-4 col-sm-4">
+
+                        <figure><img src="images/feature/app-in-nexus.png" alt="Nexus"></figure>
+
+                    </div><!-- col-md-4 -->
+
+                    <div class="col-md-8 col-sm-8">
+
+                        <div class="feature-row">
+
+                            <div class="row">
+                                <div class="col-md-6 col-xs-6 wow fadeInLeft">
+                                    <i class='pe-7s-loop'></i>
+                                    <h3>Innovacion constante</h3>
+                                    <p>
+                                    Innovar implica compartir, estar dispuesto a aprender de los demás y con los demás, de los compañeros y de los propios estudiantes.
+                                    </p>
+                                </div>
+
+                                <div class="col-md-6 col-xs-6 wow fadeInRight">
+                                    <i class='pe-7s-leaf'></i>
+                                    <h3>Compromiso con la Naturaleza</h3>
+                                    <p>
+                                    En nuestro centro, que somos muy conscientes del impacto que tiene la actividad humana en el planeta y que estamos naturalmente comprometidos a minimizarlo.
+                                    </p>
+                                </div>
+
+                            </div> <!-- row -->
+
+                        </div><!-- feature row -->
+
+                        <div class="feature-row">
+
+                            <div class="row">
+
+                                <div class="col-md-6 col-xs-6 wow fadeInLeft" data-wow-delay="1s">
+                                    <i class='pe-7s-compass'></i>
+                                    <h3>Orientacion Escolar</h3>
+                                    <p>
+                                    El orientador escolar debe atentder a todos los actores de la comunidad educativa, debe ser capáz de crear la empatía con todos y cada uno de ellos
+                                        su rol se basa en el amor y en la práctica de la cultura de la paz. 
+                                    </p>
+                                </div>
+
+                                <div class="col-md-6 col-xs-6 wow fadeInRight" data-wow-delay="1s">
+                                    <i class='pe-7s-coffee'></i>
+                                    <h3>Convivencias</h3>
+                                    <p>
+                                    A partir de las interrelaciones entre diferentes miembros de la comunidad educativa que se incide de manera significativa en el desarrollo ético e intelectual de los estudiantes.
+                                    </p>
+                                </div>
+
+                            </div><!-- row -->
+
+                        </div><!-- feature row -->
+
+                    </div><!-- col-md-8 -->
+
+                </div><!-- row -->
+
+            </div><!-- container -->
+
+        </div><!-- section-container -->
+
+    </section><!-- feature -->
+
+    <!-- ############################ Teams ############################ -->
+    <section id="team" class="section light">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-6 col-sm-7 col-md-offset-1">
+
+                    <div class="section-heading-container-right">
+                        <h2>
+                            Nuestro <span> Equipo</span>.
+                        </h2>
+
+                        <p class="text-align: justify;">
+                        Nos preocupamos por contar con personal calificado, brindando oportunidades de capacitación, crecimiento profesional y estabilidad.
+                        Estamos orgullosos de cada persona que conforma el grupo de trabajo todos profesionales en diferentes áreas, trabajando en pro de ofrecer un servicio
+                        de calidad que siempre está en proceso de mejoramiento continuo, para que nuestros estudiantes sean los mayores beneficiados.
+                        </p>
+
+                    </div>
+
+                    <div class="team-list">
+                        
+                        <ul>
+                            <li>
+                                <a href="#" data-team="member-1">
+                                    <figure>
+                                        <img src="images/team/team5.png" alt="Ty Martin">
+                                    </figure>
+                                    <span></span>
+                                    <p>Mario Castañeda</p>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="#"  class="active" data-team="member-2">
+                                    <figure>
+                                        <img src="images/team/team6.png" alt="Ezra Fortin">
+                                    </figure>
+                                    <span></span>
+                                    <p>Erick Lopez</p>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="#" data-team="member-3">
+                                    <figure>
+                                        <img src="images/team/team7.png" alt="Taylor Morin">
+                                    </figure>
+                                    <span></span>
+                                    <p>Alberto Gonzalez</p>
+                                </a>
+                            </li>
+                        </ul>
+
+                    </div><!-- team-list -->
+                    
+                </div><!-- col-md-6 -->
+
+                <div class="col-md-3 col-sm-4 col-sm-offset-1 col-md-offset-1">
+
+                    <div id="member-1" class="animated fadeIn team-display">
+                        <figure>
+                            <img src="images/team/team5.png" alt="Ty Martin">
+                        </figure>
+
+                        <div class="details">
+                            <h4>Mario Castañeda</h4>
+                            <p>Catedratico</p>
+                            <div class="social">
+                                <a href="#"> <i class="fa fa-youtube"></i> </a>
+                                <a href="#"> <i class="fa fa-github"></i> </a>
+                                <a href="#"> <i class="fa fa-slack"></i> </a>
+                                <a href="#"> <i class="fa fa-facebook"></i> </a>
+                            </div>
+                        </div><!-- details -->
+
+                    </div><!-- team-display -->
+
+                    <div id="member-2" class="animated fadeIn team-display active">
+                        <figure>
+                            <img src="images/team/team6.png" alt="Ezra Fortin">
+                        </figure>
+
+                        <div class="details">
+                            <h4>Erick Lopez</h4>
+                            <p>Catedratico</p>
+                            <div class="social">
+                                <a href="#"> <i class="fa fa-github"></i> </a>
+                                <a href="#"> <i class="fa fa-slack"></i> </a>
+                                <a href="#"> <i class="fa fa-youtube"></i> </a>
+                                <a href="#"> <i class="fa fa-facebook"></i> </a>
+                            </div>
+                        </div><!-- details -->
+
+                    </div><!-- team-display -->
+
+                    <div id="member-3" class="animated fadeIn team-display">
+                        <figure>
+                            <img src="images/team/team7.png" alt="Taylor Morin">
+                        </figure>
+
+                        <div class="details">
+                            <h4>Alberto Gonzalez</h4>
+                            <p>Supervisor</p>
+                            <div class="social">
+                                <a href="#"> <i class="fa fa-github"></i> </a>
+                                <a href="#"> <i class="fa fa-slack"></i> </a>
+                                <a href="#"> <i class="fa fa-youtube"></i> </a>
+                                <a href="#"> <i class="fa fa-facebook"></i> </a>
+                            </div>
+                        </div><!-- details -->
+
+                    </div><!-- team-display -->
+
+                </div><!-- col-md-3 -->
+
+            </div><!-- row -->
+
+        </div><!-- container -->
+        
+
+    </section><!-- Team section -->
+
+
+    <!-- ############################ Footer ############################ -->
+    <footer class="section dark" id="footer">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-12">
+                    <div class="section-heading-container">
+                        <h2>
+                            ¿Quieres <span>comunicarte</span> con nostros?
+                        </h2>
+                    </div><!-- section-header-container -->
+                </div><!-- col-md-12 -->
+
+                <div class="col-md-12">
+                    <div class="social-icons text-center">
+                        <ul>
+                            <li><a href="#"> <i class="fa fa-facebook"></i> </a></li>
+                            <li><a href="#"> <i class="fa fa-instagram"></i> </a></li>
+                            <li><a href="#"> <i class="fa fa-twitter"></i> </a></li>
+                            <li><a href="#"> <i class="fa fa-youtube"></i> </a></li>
+                        </ul>
+
+                        <p class="social-text">
+                            Siguenos!
+                        </p><!-- social-text -->
+                    </div><!-- social-icons -->
+                </div><!-- col-md-12 -->
+
+                <div class="col-md-12">
+                    <div class="copyright text-center">
+                        <p>&copy; <?php echo date("Y"); ?>  ClassLearn - Todos los derechos Reservados</p>
+                    </div><!-- copyright -->
+                </div><!-- col-md-12 -->
+
+            </div><!-- row -->
+        </div><!-- container -->
+    </footer>
+
+
+
+    <!-- Scroll to Top
+    ======================= -->
+    <a href="#" id="scroll-to-top">Scroll to Top</a>
+    <!-- Scripts -->
+    <script src="../../../ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.3.min.js"><\/script>')</script> 
+    <script src="js/vendor/owl.carousel.js"></script>
+    <script src="js/vendor/venobox.min.js"></script>
+    <script src="js/vendor/jquery.waypoints.min.js"></script>
+    <script src="js/vendor/jquery.countTo.js"></script>
+    <script src="js/vendor/imagesloaded.pkgd.min.js"></script>
+    <script src="js/vendor/masonry.pkgd.min.js"></script>
+    <script src="js/vendor/readmore.min.js"></script>
+    <script src="js/vendor/wow.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+<!-- Mirrored from devscorner.com/theme/ova/ by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 14 Mar 2020 20:15:01 GMT -->
+</html>
